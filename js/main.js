f37e2c7654b601a0c8e60f919308544e7370a6d8
galyverasi <galyverasi@gmail.com>
2021-10-31 15:23:04 -0600

    bonus

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
 let display = document.querySelector(".display")
 let plus = document.querySelector(".plus")
 let minus = document.querySelector(".minus")
-
+let numInput = document.querySelector("input[type='number']")
 let count = 0
 
 updateDisplay()
-
+// add and keep color to black
 plus.addEventListener("click",()=>{
-    count++;
+    count++
     updateDisplay()
+    if (count>0) {
+        display.style.color = "black"
+    }
 }) 
-
+// subtract and keep color to red
 minus.addEventListener("click",()=>{
     count--
     updateDisplay()
-})
+    let newNum = numInput.value
+    let minus = document.querySelector(".minus")
+    if (minus) {
+        let updateDisplay = -(newNum-1)
+        display.style.color = "red"
+        display.innerHTML = updateDisplay
+    }
 
+})
 function updateDisplay(){
     display.innerHTML = count;
-    
 }
